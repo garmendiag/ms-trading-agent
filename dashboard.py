e53ac7a1f46f1f6f8cf3ba427c79b27e5d785699
Rafael Guzman <raguzmang@gmail.com>
2025-03-14 17:09:43 -0400

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -465,6 +465,7 @@ def create_signal_chart(data):
                 size=12,
                 symbol='triangle-up',
                 line=dict(width=1, color='black')
-            )),
+            ),
             text=bullish_signals['signal'],
-          hovertemplate='%{text}<br>{Confidence: %}<response clipped> <NOTE>To save on context only part of this file has been shown to you. You should retry this tool after you have searched inside the file with `grep -n` in order to find the line numbers of what you are looking for.</NOTE>
+          hovertemplate='%{text}<br>{Confidence: %} <response clipped> <NOTE>To save on context only part of this file has been shown to you. You should retry this tool after you have searched inside the file with `grep -n` in order to find the line numbers of what you are looking for.</NOTE>
+      )
